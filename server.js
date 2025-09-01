a222c93c0cbbf0e0b82508e80a034fb7d6c11f92
joaokafer <joaopedrokafer@gmail.com>
2025-09-01 20:53:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+const apostasClientes = new Map();
+const configLoteria = {inicio: 0, fim: 100, qtd:5};
 // Evento de conexão
 io.on('connection', (socket) => {
   console.log('Um cliente conectado!');
@@ -25,12 +26,42 @@ io.on('connection', (socket) => {
   // Envia a mensagem de boas-vindas ao cliente
   const timestamp = new Date().toLocaleTimeString();
   socket.emit('msg1', `${timestamp}: CONECTADO!!`);
-
+socket.on('enviar_comando', (mensagem) => {
+    if (mensagem.startsWith(':')) {
+      const [comando, valor] = mensagem.split(' ');
+      // Modifica a variável configLoteria que está lá fora
+      if (comando === ':inicio') configLoteria.inicio = parseInt(valor);
+      if (comando === ':fim') configLoteria.fim = parseInt(valor);
+      if (comando === ':qtd') configLoteria.qtd = parseInt(valor);
+      console.log('Configuração da loteria atualizada:', configLoteria);
+    } else {
+      const numeros = mensagem.split(' ').map(n => parseInt(n));
+      apostasClientes.set(socket.id, numeros);
+    }
+  });
   // Lidar com desconexões
   socket.on('disconnect', () => {
     console.log('Cliente desconectado.');
   });
 });
+function realizarSorteio() {
+  console.log('--- REALIZANDO SORTEIO ---');
+  console.log('Configuração atual:', configLoteria); // Agora vai funcionar!
+
+  const { inicio, fim, qtd } = configLoteria;
+  const numerosSorteados = new Set();
+
+  while (numerosSorteados.size < qtd) {
+    const numero = Math.floor(Math.random() * (fim - inicio + 1)) + inicio;
+    numerosSorteados.add(numero);
+  }
+
+  const resultado = Array.from(numerosSorteados);
+  console.log('Números sorteados:', resultado);
+}
+
+// Inicia o timer
+setInterval(realizarSorteio, 60000);
 
 // Inicia o servidor
 server.listen(PORT, () => {
